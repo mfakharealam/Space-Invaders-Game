af6435d98caf0baa8ddf7bf678edbe17c1abed9c
MFA571 <fakhar571@gmail.com>
2019-08-15 23:24:20 +0500

    added gun firing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import turtle
 import os
 
+turtle.tracer(1)
+turtle.setundobuffer(1)
+
 # Setting up screen
 win = turtle.Screen()
 win.bgcolor("black")
@@ -41,6 +44,24 @@ invader.setposition(-200, 250)
 
 invader_speed = 2
 
+# Player's gunfire
+
+gunfire = turtle.Turtle()
+gunfire.color("yellow")
+gunfire.shape("triangle")
+gunfire.penup()
+gunfire.speed(0)
+gunfire.setheading(90)
+gunfire.shapesize(0.5, 0.5)
+gunfire.hideturtle()
+
+gunfire_speed = 20
+
+# State of the gun
+
+gunfire_state = "ready"     # when game is started
+
+
 # Movement of player
 
 
@@ -60,13 +81,23 @@ def right_movement(event):
     player.setx(x)
 
 
+def gunfiring():
+    global gunfire_state    # any changes here are reflected everywhere to this var
+    if gunfire_state == "ready":
+        gunfire_state = "fire"
+        x = player.xcor()
+        y = player.ycor() + 10
+        gunfire.setposition(x, y)
+        gunfire.showturtle()
+
+
 # Keyboard bindings
 # win.onkey(left_movement, "Left")
 # win.onkey(right_movement, "Right")
 # in order to keep turtle moving continuously without multiple presses
 turtle.getcanvas().bind('<Right>', right_movement)
 turtle.getcanvas().bind('<Left>', left_movement)
-
+turtle.onkey(gunfiring, "space")
 win.listen()
 
 # Main loop
@@ -83,5 +114,14 @@ while True:
         invader_speed *= -1
         invader.sety(invader.ycor() - 40)
 
+    # moving the gunfire
+    if gunfire_state == "fire":
+        gunfire.sety(gunfire.ycor() + gunfire_speed)
+
+    # gunfire border
+
+    if gunfire.ycor() > 275:
+        gunfire_state = "ready"
+        gunfire.hideturtle()
 
 win.mainloop()
